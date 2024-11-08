19e89bc9b3718fa5796f57cdc49bfbf9c3ac2f30
Tomas Vergara <tominacho@hotmail.com>
2024-11-08 12:03:17 -0300

    Initial commit

---
+++ src/main/java/utn/frc/ppai/bonvino/EbdfPpaiBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package utn.frc.ppai.bonvino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdfPpaiBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdfPpaiBackendJavaApplication.class, args);
+	}
+
+}
