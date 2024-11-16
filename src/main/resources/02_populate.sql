6edca45c16499ee9f6b39de50b3a122108bb1831
Tomas Vergara <tominacho@hotmail.com>
2024-11-16 14:21:16 -0300

    Se actualizaron las fechas para que sean coherentes con las actualizaciones disponibles.

--- src/main/resources/02_populate.sql
+++ src/main/resources/02_populate.sql
@@ -1,16 +1,16 @@
 -- IMPORTANTE: Las cosnultas se deben ejecutar si o si en el orden indicado:
 
 INSERT INTO bodega (id, nombre, descripcion, historias, periodo_actualizacion, ultima_actualizacion, img_logo_bodega, sitio_web, latitud, longitud) VALUES
-                (1, 'Bodega Freixenet Argentina', 'Bodega especializada en espumantes.', 'Historia de Freixenet Argentina.', 12, '2023-05-15', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/19_799412406012b67f5141fa74399575e0.jpg', 'http://www.freixenet.com.ar', -34.4713, -58.5555),
-                (2, 'Bodega Flechas De Los Andes', 'Bodega ubicada en Mendoza con enfoque en vinos de alta gama.', 'Historia de Flechas De Los Andes.', 6, '2023-12-10', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/159_b05db0957d4f6eeac441c8f6358d17a2.jpg', 'http://www.flechasdelosandes.com.ar', -32.8898, -68.8458),
-                (3, 'Bodega La Caroyense', 'Bodega tradicional de Córdoba.', 'Historia de La Caroyense.', 3, '2024-02-20', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/155_334fb1dd99c20e75fe0df768f9198cf5.jpg', 'http://www.bodegalacaroyense.com.ar', -31.0441, -64.1081),
-                (4, 'Bodega Domaine Le Billoud', 'Bodega boutique en Mendoza.', 'Historia de Domaine Le Billoud.', 12, '2023-10-05', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/37_594412dbcbeee4e681b4c0288add8415.jpg', 'http://www.domainelebilloud.com', -32.8975, -68.835),
-                (5, 'Bodega Lockton Argentina', 'Consultora de seguros y riesgos.', 'Historia de Lockton Argentina.', 12, '2024-01-01', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/282_519370a8481d96663b06f19bc1db05d6.jpg', NULL, -34.5625, -58.4428),
-                (6, 'Bodega L&L Servicios Internacionales', 'Servicios de importación y exportación.', 'Historia de L&L Servicios Internacionales.', 1, '2024-04-18', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/271_5aefbb8454d0657fa18c2a84c6c760cf.png', NULL, -32.8899, -68.8456),
-                (7, 'Bodega Luis Segundo Correas', 'Bodega con tradición familiar.', 'Historia de Luis Segundo Correas.', 12, '2023-09-25', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/29_608812743f3e95b3a9a0238bada1181c.jpg', 'http://www.bodegacorreas.com.ar', -32.8898, -68.8458),
-                (8, 'Bodega Aaron Tubert', 'Pequeña bodega familiar.', 'Historia de Aaron Tubert.', 3, '2024-02-18', 'https://www.bodegasdeargentina.org/wp-content/uploads/partners/193_42d2e3a3f09caa42233d10c04931c5d0.jpg', NULL, -32.8898, -68.8458),
-                (9, 'Bodega Dante Robino', 'Bodega en Buenos Aires.', 'Historia de Dante Robino.', 12, '2024-04-05', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/193_42d2e3a3f09caa42233d10c04931c5d0.jpg', 'http://www.bodegadanterobino.com', -34.6022, -58.378),
-                (10, 'Bodega Lagarde', 'Bodega con vinos premiados.', 'Historia de Lagarde.', 12, '2024-03-01', 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/27_772be6bee3c1b94099c4c5311fdd0b5e.jpg', 'http://www.rocawines.com', -32.9831, -68.8756);
+                (1, 'Bodega Freixenet Argentina', 'Bodega especializada en espumantes.', 'Historia de Freixenet Argentina.', 12, 1681257600000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/19_799412406012b67f5141fa74399575e0.jpg', 'http://www.freixenet.com.ar', -34.4713, -58.5555),
+                (2, 'Bodega Flechas De Los Andes', 'Bodega ubicada en Mendoza con enfoque en vinos de alta gama.', 'Historia de Flechas De Los Andes.', 6, 1723075200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/159_b05db0957d4f6eeac441c8f6358d17a2.jpg', 'http://www.flechasdelosandes.com.ar', -32.8898, -68.8458),
+                (3, 'Bodega La Caroyense', 'Bodega tradicional de Córdoba.', 'Historia de La Caroyense.', 3, 1705795200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/155_334fb1dd99c20e75fe0df768f9198cf5.jpg', 'http://www.bodegalacaroyense.com.ar', -31.0441, -64.1081),
+                (4, 'Bodega Domaine Le Billoud', 'Bodega boutique en Mendoza.', 'Historia de Domaine Le Billoud.', 12, 1704499200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/37_594412dbcbeee4e681b4c0288add8415.jpg', 'http://www.domainelebilloud.com', -32.8975, -68.835),
+                (5, 'Bodega Lockton Argentina', 'Consultora de seguros y riesgos.', 'Historia de Lockton Argentina.', 12, 1704067200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/282_519370a8481d96663b06f19bc1db05d6.jpg', NULL, -34.5625, -58.4428),
+                (6, 'Bodega L&L Servicios Internacionales', 'Servicios de importación y exportación.', 'Historia de L&L Servicios Internacionales.', 1, 1710489600000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/271_5aefbb8454d0657fa18c2a84c6c760cf.png', NULL, -32.8899, -68.8456),
+                (7, 'Bodega Luis Segundo Correas', 'Bodega con tradición familiar.', 'Historia de Luis Segundo Correas.', 12, 1706227200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/29_608812743f3e95b3a9a0238bada1181c.jpg', 'http://www.bodegacorreas.com.ar', -32.8898, -68.8458),
+                (8, 'Bodega Aaron Tubert', 'Pequeña bodega familiar.', 'Historia de Aaron Tubert.', 3, 1702944000000, 'https://www.bodegasdeargentina.org/wp-content/uploads/partners/193_42d2e3a3f09caa42233d10c04931c5d0.jpg', NULL, -32.8898, -68.8458),
+                (9, 'Bodega Dante Robino', 'Bodega en Buenos Aires.', 'Historia de Dante Robino.', 12, 1707177600000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/193_42d2e3a3f09caa42233d10c04931c5d0.jpg', 'http://www.bodegadanterobino.com', -34.6022, -58.378),
+                (10, 'Bodega Lagarde', 'Bodega con vinos premiados.', 'Historia de Lagarde.', 12, 1709251200000, 'https://www.bodegasdeargentina.org//wp-content/uploads/partners/27_772be6bee3c1b94099c4c5311fdd0b5e.jpg', 'http://www.rocawines.com', -32.9831, -68.8756);
 
 INSERT INTO tipo_uva (nombre, descripcion) VALUES
                    ('Tempranillo', 'Variedad de uva tinta utilizada para hacer vino en España y Portugal.'),
@@ -156,33 +156,32 @@ INSERT INTO usuario (nombre, password) VALUES ('migueld', 'password123');
 INSERT INTO enofilo (apellido, nombre, imagen_perfil, usuario_id)
 VALUES ('Diaz', 'Miguel', 'https://randomuser.me/api/portraits', (SELECT id FROM usuario WHERE nombre = 'migueld'));
 
--- Inserts para la tabla siguiendo
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Juan' AND apellido = 'Gonzalez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Juan' AND apellido = 'Gonzalez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Maria' AND apellido = 'Perez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Maria' AND apellido = 'Perez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega Aaron Tubert'), (SELECT id FROM enofilo WHERE nombre = 'Pedro' AND apellido = 'Gomez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega Aaron Tubert'), (SELECT id FROM enofilo WHERE nombre = 'Pedro' AND apellido = 'Gomez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Ana' AND apellido = 'Rodriguez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Ana' AND apellido = 'Rodriguez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega L&L Servicios Internacionales'), (SELECT id FROM enofilo WHERE nombre = 'Lucia' AND apellido = 'Fernandez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega L&L Servicios Internacionales'), (SELECT id FROM enofilo WHERE nombre = 'Lucia' AND apellido = 'Fernandez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega Aaron Tubert'), (SELECT id FROM enofilo WHERE nombre = 'Carlos' AND apellido = 'Lopez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega Aaron Tubert'), (SELECT id FROM enofilo WHERE nombre = 'Carlos' AND apellido = 'Lopez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Sofia' AND apellido = 'Martinez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Sofia' AND apellido = 'Martinez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega L&L Servicios Internacionales'), (SELECT id FROM enofilo WHERE nombre = 'Jorge' AND apellido = 'Sanchez'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega L&L Servicios Internacionales'), (SELECT id FROM enofilo WHERE nombre = 'Jorge' AND apellido = 'Sanchez'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Valeria' AND apellido = 'Romero'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega Freixenet Argentina'), (SELECT id FROM enofilo WHERE nombre = 'Valeria' AND apellido = 'Romero'));
 
 INSERT INTO siguiendo (fecha_inicio, bodega_id, enofilo_id)
-VALUES ('2020-01-01', (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Miguel' AND apellido = 'Diaz'));
+VALUES (1577836800000, (SELECT id FROM bodega WHERE nombre = 'Bodega La Caroyense'), (SELECT id FROM enofilo WHERE nombre = 'Miguel' AND apellido = 'Diaz'));
